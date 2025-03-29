4ab26d130502910dbecc5432fefaa7e5f67081ed
jorgirerison <jorgipeixoto@alu.ufc.br>
2025-03-29 19:08:47 -0300

    creation all product routes

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerce.db'
 
 db = SQLAlchemy(app)
+CORS(app)
 
 class Product(db.Model):
   id = db.Column(db.Integer, primary_key=True)
@@ -20,7 +22,61 @@ def add_product():
     db.session.add(product)
     db.session.commit()
     return jsonify({"message": "Product added successfully"})
-  return jsonify({"message": "Invalid product data"}), 400
+  return jsonify({"message": "Failed to add the product"}), 400 # solicitação malformada
+
+@app.route('/api/products/delete/<int:product_id>', methods = ["DELETE"])
+def delete_product(product_id):
+  product = Product.query.get(product_id)
+  if product:
+    db.session.delete(product)
+    db.session.commit()
+    return jsonify({"message": "Product deleted successfully"})
+  return jsonify({"message": "Not Found. Product not available"}), 404 # não conseguiu encontrar o recurso
+
+@app.route('/api/products/<int:product_id>', methods = ["GET"])
+def get_product_details(product_id):
+  product = Product.query.get(product_id)
+  if product:
+    return jsonify({
+      "id": product.id,
+      "name": product.name,
+      "price": product.price,
+      "description": product.description,
+      })
+  return jsonify({"message": "Not Found. Product not available"}), 404
+
+@app.route('/api/products/update/<int:product_id>', methods = ["PUT"])
+def update_product(product_id):
+  product = Product.query.get(product_id)
+  if not product:
+    return jsonify({"message": "Not Found. Product not available"}), 404 
+
+  data = request.json
+  if 'name' in data:
+    product.name = data['name']
+  
+  if 'price' in data:
+    product.price = data['price']
+
+  if 'description' in data:
+    product.description = data['description']
+
+  db.session.commit()
+  return jsonify({"message": "Product update successfully"})
+
+@app.route('/api/products', methods=["GET"])
+def get_products():
+  products = Product.query.all()
+  product_list = []
+  for product in products:
+    product_data = ({
+      "id": product.id,
+      "name": product.name,
+      "price": product.price,
+    })
+    product_list.append(product_data)
+  return jsonify(product_list)
+
 
 @app.route('/')
 def hello_world():
